b1ab1e484eea2df9a330615a71e1bf8432f57ef7
Yachiru <81792155+YachiJishi@users.noreply.github.com>
2024-07-16 23:30:37 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,33 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    constructor(uint initBalance) payable {
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
         require(msg.sender == owner, "You are not the owner of this account");
 
+        uint256 _previousBalance = balance;
         balance += _amount;
-
         assert(balance == _previousBalance + _amount);
 
         emit Deposit(_amount);
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
@@ -35,7 +32,8 @@ contract Assessment {
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -44,9 +42,18 @@ contract Assessment {
         }
 
         balance -= _withdrawAmount;
-
-        assert(balance == (_previousBalance - _withdrawAmount));
+        assert(balance == _previousBalance - _withdrawAmount);
 
         emit Withdraw(_withdrawAmount);
+    }
+
+    function transferOwnership(address payable newOwner) public {
+        require(newOwner != address(0), "Invalid new owner address");
+        require(msg.sender == owner, "You are not the owner of this contract");
+
+        address payable previousOwner = owner;
+        owner = newOwner;
+
+        emit OwnershipTransferred(previousOwner, newOwner);
     }
 }
